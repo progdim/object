f7182da01a0291a36735db87456e5e8640f18298
Admin <dmtr2001@mail.ru>
2022-11-12 19:10:02 +0700

    commit

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -3,10 +3,12 @@ package com.company;
 public class Book {
     private String nameBook;
     private int yearCreation;
+    private Author author;
 
-    public Book (String nameBook, int yearCreation){
+    public Book (String nameBook, int yearCreation, Author author){
         this.nameBook = nameBook;
         this.yearCreation = yearCreation;
+        this.author = author;
     }
 
     public String getNameBook() {
@@ -20,5 +22,9 @@ public class Book {
 
     public void setYearCreation(int yearCreation) {
         this.yearCreation = yearCreation;
+    }
+
+    public Author getAuthor() {
+        return author;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Author lev = new Author("Lev","Tolstoy");
-        Author vladimir = new Author("Vladimir","Lenin");
+        Author lev = new Author("Lev", "Tolstoy");
+        Author vladimir = new Author("Vladimir", "Lenin");
 
-        Book nameBook1 = new Book("Война и мир", 1863);
+        Book nameBook1 = new Book("Война и мир", 1863,lev);
         nameBook1.setYearCreation(1872);
-        Book nameBook2 = new Book("Что делать?", 1899);
-//        System.out.println(lev.nameAutor + " " + lev.surnameAutor);
+        Book nameBook2 = new Book("Что делать?", 1899,vladimir);
+ //       System.out.println(nameBook1);
 //        System.out.println(vladimir.nameAutor + " " + vladimir.surnameAutor);
 //
 //        System.out.println(nameBook1.nameBook + " " + nameBook1.yearCreation);
 
 
-       String finBook1 = lev.getNameAutor() +" "+ lev.getSurnameAutor() +" "+ nameBook1.getNameBook() +" "+ nameBook1.getYearCreation();
-       String finBook2 = vladimir.getNameAutor() +" "+ vladimir.getSurnameAutor() +" "+ nameBook2.getNameBook()+" "+nameBook2.getYearCreation();
+        String finBook1 = nameBook1.getAuthor().getNameAutor() + " " + nameBook1.getAuthor().getSurnameAutor() + " " + nameBook1.getNameBook() + " " + nameBook1.getYearCreation();
+        String finBook2 = nameBook2.getAuthor().getNameAutor() + " " + nameBook2.getAuthor().getSurnameAutor() + " " + nameBook2.getNameBook() + " " + nameBook2.getYearCreation();
         System.out.println("Книга первая: " + finBook1);
         System.out.println("Книга вторая: " + finBook2);
     }
 }
+
+
